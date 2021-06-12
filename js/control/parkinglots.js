8281f71890f943990c4ae1b8e75c6f5e008bfec6
quithu165 <ngquithu@gmail.com>
2021-06-12 17:00:12 +0700

    almost done 1

--- js/control/parkinglots.js
+++ js/control/parkinglots.js
@@ -58,7 +58,8 @@ function getOwnedParkingLotsList() {
         var totalarea = data[i].area.length;
         for (var j = 0; j < totalarea; j++) {
           totalslot =
-            totalslot + data[i].area.freeslot + data[i].area[j].fullslot;
+            totalslot + data[i].area[j].freeslot + data[i].area[j].fullslot;
+            // console.log(totalslot);
         }
         createNewRow(
           data[i]._id,
