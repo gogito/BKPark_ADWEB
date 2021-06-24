0e65eacfd78766bb9e6fccdbab9890aa809501f1
quithu165 <ngquithu@gmail.com>
2021-06-24 21:00:31 +0700

    add chart

--- js/demo/datatables-demo.js
+++ js/demo/datatables-demo.js
@@ -1,12 +1,12 @@
 // Call the dataTables jQuery plugin
 $(document).ready(function () {
-  function showpanel() {
-    $('#dataTable').DataTable();
-  };
-
-  // use setTimeout() to execute
-  setTimeout(showpanel, 7000)
-});
-
-
-// Call the dataTables jQuery plugin
+    function showpanel() {
+      $('#dataTable').DataTable();
+    };
+  
+    // use setTimeout() to execute
+    setTimeout(showpanel, 7000)
+  });
+  
+  
+  // Call the dataTables jQuery plugin
